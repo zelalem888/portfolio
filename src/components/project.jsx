a02e76429f7a8ba73053cd69a4781a4d65134d3f
Zelalem Legesse <legessezelalem888@gmail.com>
2025-10-16 01:19:50 +0300

    the card began

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,6 +1,6 @@
 import ScrollFloat from "../assets/ScrollFloat/ScrollFloat";
 import TiltedCard from "../assets/Title-Card/Title-Card";
-import  "../assets/Title-Card/ TiltedCards.css"
+import "../assets/Title-Card/ TiltedCards.css";
 const Project = () => {
   return (
     <div className="grid grid-cols-3 w-[80%] mx-auto gap-y-10">
@@ -13,23 +13,33 @@ const Project = () => {
 };
 const TiltedCards = () => {
   return (
-    <TiltedCard
-      imageSrc="/assets/photo/20250823_084014.jpg"
-      altText="Zelalem Legesse"
-      captionText="Zelalem"
-      containerHeight="300px"
-      containerWidth="270px"
-      imageHeight="300px"
-      imageWidth="270px"
-      rotateAmplitude={12}
-      scaleOnHover={1.2}
-      showMobileWarning={false}
-      showTooltip={true}
-      displayOverlayContent={true}
-      overlayContent={
-        <p className="tilted-card-demo-text">Kendrick Lamar - GNX</p>
-      }
-    />
+    <div>
+      <TiltedCard
+        imageSrc="/assets/photo/20250823_084014.jpg"
+        altText="Zelalem Legesse"
+        captionText="Zelalem"
+        containerHeight="300px"
+        containerWidth="270px"
+        imageHeight="300px"
+        imageWidth="270px"
+        rotateAmplitude={12}
+        scaleOnHover={1.2}
+        showMobileWarning={false}
+        showTooltip={true}
+        displayOverlayContent={true}
+        overlayContent={
+          <>
+            <div className="grid grid-rows-2 content-between h-[290px]">
+              <p className="tilted-card-demo-text">Kendrick Lamar - GNX</p>
+              <div className="flex justify-evenly gap-5 self-end ">
+                <a href=""><i className="bi bi-github text-2xl text-white"></i></a>
+                <a href=""><i className="bi bi-box-arrow-up-right text-2xl text-white"></i></a>
+              </div>
+            </div>
+          </>
+        }
+      />
+    </div>
   );
 };
 
